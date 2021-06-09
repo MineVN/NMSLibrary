e1fa5c8280f0fc805d973e6799e6125dab98d078
tranlong5252 <longpham5252@gmail.com>
2021-06-10 02:19:33 +0700

    debugged

--- NMSLibrary_VERSION/src/main/java/remvn/nmslibrary/version_api/NBT.java
+++ NMSLibrary_VERSION/src/main/java/remvn/nmslibrary/version_api/NBT.java
@@ -6,57 +6,57 @@ public interface NBT {
 	
     /**
      * Adding string tag from ItemStack
-     * @param item
-     * @param key
-     * @param value
+     * @param item item
+     * @param key key
+     * @param value value
      */
     void addStringTag(ItemStack item, String key, String value);
     
     /**
      * Adding int tag to ItemStack
-     * @param item
-     * @param key
-     * @param value
+     * @param item item
+     * @param key key
+     * @param value value
      */
     void addIntTag(ItemStack item, String key, int value);
     
     /**
      * Adding Double tag to ItemStack
-     * @param item
-     * @param key
-     * @param value
+     * @param item item
+     * @param key key
+     * @param value value
      */
     void addDoubleTag(ItemStack item, String key, double value);
 
     /**
      * check if tag exist with key
-     * @param item
-     * @param key
-     * @return
+     * @param item item
+     * @param key key
+     * @return haskey
      */
     boolean hasKey(ItemStack item, String key);
 
     /**
      * Getting int tag from ItemStack
-     * @param item
-     * @param key
-     * @return
+     * @param item item
+     * @param key key
+     * @return inttag
      */
     int getIntTag(ItemStack item, String key);
     
     /**
      * Getting String tag from ItemStack
-     * @param item
-     * @param key
-     * @return
+     * @param item item
+     * @param key key
+     * @return stringtag
      */
     String getStringTag(ItemStack item, String key);
     
     /**
      * Getting Double tag from ItemStack
-     * @param item
-     * @param key
-     * @return
+     * @param item item
+     * @param key key
+     * @return doubletag
      */
     double getDoubleTag(ItemStack item, String key);
     
